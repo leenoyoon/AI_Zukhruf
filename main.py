91bae75481a1d683269db5e89197a5e4db96f996
leen Oyoon <leenoyoon2@gmail.com>
2026-04-12 23:31:24 +0300

    Completed Phase 2 Texture Synthesis & SSD Matching

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ from core.patch_bank import (
     get_best_initial_patch
 )
 
+from core.ssd_matching import generate_texture_canvas
+
 if __name__ == "__main__":
-    image_path = r"C:\Users\ASUS\Desktop\pattern1.jpg" 
+    image_path = r"data\input_images\pattern16.jpg" 
     original_img = cv2.imread(image_path)
     
     if original_img is not None:
@@ -25,7 +27,8 @@ if __name__ == "__main__":
         smooth_mask = morphological_close(mask_img)
         final_depth_map = apply_mask(equalized_img, smooth_mask)
         
-        patch_bank = build_patch_bank(final_depth_map, patch_size=64, overlap_ratio=0.25)
+        patch_size=64
+        patch_bank = build_patch_bank(final_depth_map, patch_size=patch_size, overlap_ratio=0.25)
         best_idx, best_patch, max_std_val = get_best_initial_patch(patch_bank)
         
         print("Best calculated threshold is:", optimal_thresh)
@@ -36,6 +39,25 @@ if __name__ == "__main__":
         cv2.imshow("2. Otsu Mask", mask_img)
         cv2.imshow("3. FINAL CNC DEPTH MAP", final_depth_map)
         cv2.imshow("4. Best Initial Patch", best_patch)
+        
+        
+        overlap_width = patch_size // 6
+        num_patches_y = 14
+        num_patches_x = 16  
+        
+        final_canvas = generate_texture_canvas(
+            patch_bank, 
+            best_patch, 
+            num_patches_y, 
+            num_patches_x, 
+            patch_size, 
+            overlap_width
+        )
+        
+        print(f"Canvas generated successfully! Final shape: {final_canvas.shape}")
+
+
+        cv2.imshow("3. GENERATED LARGE CANVAS (Phase 2)", final_canvas)
         
         cv2.waitKey(0)
         cv2.destroyAllWindows()
